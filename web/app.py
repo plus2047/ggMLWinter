e16b929f1209a9477fb5d5b63cf4422ab6317263
Xu Jiahao <xujiahao@giska.com>
2020-01-16 16:27:04 +0800

    web

--- web/app.py
+++ web/app.py
@@ -5,20 +5,34 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 from flask import url_for
+import pathlib
 
 app = flask.Flask(__name__)
 app.secret_key = "this is a secret key."
 
+DEFAULT_SENTENCE = "大江东去，浪淘尽，千古风流人物。"
+
+char_set = set()
+
+for f in pathlib.Path("static/font").glob("*.png"):
+    try:
+        char_set.add(int(f.stem))
+    except:
+        pass
 
 class InputForm(FlaskForm):
-    string = TextAreaField("Input", validators=[DataRequired()])
+    string = TextAreaField("Input", validators=[DataRequired()], default=DEFAULT_SENTENCE)
     submit = SubmitField('submit')
+
+
+def get_pic_list(s):
+    return [url_for("static", filename="font/%s.png" % ord(w)) for w in s if ord(w) in char_set]
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     form = InputForm()
     if form.validate_on_submit():
-        pic_list = [url_for("static", filename="Lantingji_Xu/%s.jpg" % i) for i in range(1, 50)]
+        pic_list = get_pic_list(form.string.data)
         return flask.render_template("index.html", form=form, pic_list=pic_list)
     return flask.render_template("index.html", form=form, pic_list=None)
